6afd1a5de96b9f93c05ad272395611ec3b745e46
Miko <89363492+MikoGome@users.noreply.github.com>
2021-08-26 02:46:17 -0700

    Update index.js

--- public/index.js
+++ public/index.js
@@ -282,7 +282,7 @@ socket.on("typing", () => {
 socket.on("away", status => {
   if (status === 'visible') {
     //clearTimeout(away1);
-    //document.getElementById("friend_box").style.transition = "all 1s";
+    document.getElementById("friend_box").style.transition = "all 1s";
     document.getElementById("friend_box").style.filter = "brightness(1)";
     document.getElementById("friend_box").style.backgroundPositionY = "110%";
   }
