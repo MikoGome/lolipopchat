ad28917c40754890e48d6f2b6f2aadefc9bda768
Miko <89363492+MikoGome@users.noreply.github.com>
2021-08-27 14:49:58 -0700

    Update index.js

--- public/index.js
+++ public/index.js
@@ -89,8 +89,6 @@ function character_choose (element) {
   element.style.width = "80%";
   document.getElementById("identity").innerHTML = element.children[1].innerHTML;
   element.children[1].outerHTML = "<label>Password: <input type='password' autofocus id='inputpassword' onkeypress='validation(event)'></label>"
-  document.getElementById("paimon").click();
-  document.getElementById("paimon").click();
 }
 
 let password1;
@@ -477,6 +475,11 @@ document.getElementById("paimon").addEventListener("click", () => {
   bgmCounter++;
   localStorage.bgmCounter = bgmCounter;
 });
+
+setTimeout( () => {
+  document.getElementById("paimon").click();
+  document.getElementById("paimon").click();
+}, 200);
 
 //==========================================================================================================
 /*
